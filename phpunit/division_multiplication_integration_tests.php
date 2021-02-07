6e9598a59a8d64049a2930065427c74040b772c9
James <james.voong@catalyst-eu.net>
2021-02-07 22:24:26 +0000

    Division multiplication integration (#3)
    * Implemented handling of division and multiplication in one equation.
    * Added unit tests for integration of division and multiplication
    * Added TODOs

---
+++ phpunit/division_multiplication_integration_tests.php
@@ -0,0 +1,62 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use Solution2\Solution;
+
+final class DivisionMultiplicationIntegrationTest extends TestCase {
+
+    public function testOneOfEachMultiplicationFirst() : void {
+        $this->assertEquals(
+            solution::eq("3*4/12"),
+            1
+        );
+    }
+
+    public function testOneOfEachDivisionFirst() : void {
+        $this->assertEquals(
+            solution::eq("100/10*5"),
+            50
+        );
+    }
+
+    public function testMultipleDivisionsOneMultiplication() : void {
+        $this->assertEquals(
+            solution::eq("1000/100/5*6"),
+            12
+        );
+    }
+
+    public function testMultipleMultiplicationsOneDivision() : void {
+        $this->assertEquals(
+            solution::eq("5*6*8/2"),
+            120
+        );
+    }
+
+    public function testMultipleOfEachDivisionFirst() : void {
+        $this->assertEquals(
+            solution::eq("72/8/3*7*4"),
+            84
+        );
+    }
+
+    public function testMultipleOfEachMultiplicationFirst() : void {
+        $this->assertEquals(
+            solution::eq("4*8*9/4/8"),
+            9
+        );
+    }
+
+    public function testMultipleOfEachMixedOrder() : void {
+        $this->assertEquals(
+            solution::eq("5*8/2*11"),
+            220
+        );
+
+        $this->assertEquals(
+            solution::eq("40/10*9*8/2"),
+            144
+        );
+    }
+
+}
--- solution2.php
+++ solution2.php
@@ -3,13 +3,68 @@
 namespace Solution2;
 
 class Solution {
-    function eq(string $exp) : int {
-        static::multiplication($exp);
+    public static function eq(string $exp) : int {
+        // Remove all whitespace from the expression.
+        $exp = str_replace(' ', '', $exp);
 
+        // Create a map so that each number and operator is its own element.
+        $map = [];
 
-        return 0;
+        // As I'll be splitting the numbers into 1 char pieces, keep track of ongoing ones.
+        $inprogressnumbers = [];
+
+        /* Attempt to create a map. It will look like this:
+        $exp = "3*4/12"
+        $map = [
+            [0] => 3,
+            [1] => 'multiply',
+            [2] => 4,
+            [3] => 'divide',
+            [4] => 12
+        ];
+        */
+        foreach (str_split($exp) as $value) {
+            switch($value) {
+                case '*':
+                    $map[] = implode($inprogressnumbers);
+                    $map[] = '*';
+                    $inprogressnumbers = [];
+                    break;
+                case '/':
+                    $map[] = implode($inprogressnumbers);
+                    $map[] = '/';
+                    $inprogressnumbers = [];
+                    break;
+                default:
+                    $inprogressnumbers[] = $value;
+                    continue;
+                    break;
+
+            }
+        }
+        // The last number isn't added into $map during the loop, add it in now.
+        $map[] = implode($inprogressnumbers);
+
+        $result = 0;
+        foreach ($map as $key => $element) {
+            if ($key == 0) {
+                $result = $element;
+            }
+            switch ($element) {
+                case '*':
+                    $result = $result * $map[$key + 1];
+                    break;
+                case '/':
+                    $result = $result / $map[$key + 1];
+                    break;
+            }
+
+        }
+
+        return $result;
     }
 
+    // TODO Assess whether still needed.
     /**
      * Takes an expression, checks if there's any multiplication, and does the multiplication.
      *
@@ -30,7 +85,7 @@ class Solution {
                     $result = $number_to_be_multiplied;
                     continue;
                 }
-
+                //TODO remove trim()
                 $result = trim($result) * trim($number_to_be_multiplied);
             }
         }
@@ -38,6 +93,7 @@ class Solution {
         return $result;
     }
 
+    // TODO Assess whether still needed.
     /**
      * Takes an expression, checks if there's any division, and does the divison.
      *
@@ -58,7 +114,7 @@ class Solution {
                     $result = $number_to_be_divided;
                     continue;
                 }
-
+                //TODO remove trim()
                 $result = trim($result) / trim($number_to_be_divided);
             }
         }
@@ -66,8 +122,10 @@ class Solution {
         return $result;
     }
 
+    // TODO Assess whether still needed.
     /**
      * Check if ready to be evaluated.
+     *
      */
     function check($array, $char) {
         $symbols = ['+', '-', '*', '/'];

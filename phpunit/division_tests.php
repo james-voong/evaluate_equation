87fff29ac52fc91ed88407050c3d4aed03fe3c19
James Voong <james.voong@catalyst-eu.net>
2021-02-07 17:29:00 +0000

    Added code and unit tests for division

--- phpunit/division_tests.php
+++ phpunit/division_tests.php
@@ -1,1 +1,22 @@
 <?php
+
+use PHPUnit\Framework\TestCase;
+use Solution2\Solution;
+
+final class DivisionTest extends TestCase {
+
+    public function testTwoParams() : void {
+        $this->assertEquals(
+            solution::division("12/4"),
+            3
+        );
+    }
+
+    public function testThreeParams() : void {
+        $this->assertEquals(
+            solution::division("125/5/5"),
+            5
+        );
+    }
+
+}

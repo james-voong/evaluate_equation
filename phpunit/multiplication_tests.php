edb15dc3aecfe9b8256c1988c9c52766c3517246
James <james.voong@catalyst-eu.net>
2021-02-07 17:06:31 +0000

    Added Multiplication (#1)
    * Started work on handling multiplication only
    * Multiplication appears to be working.
    * Added unit tests for multiplication.
    * Tidied up solution2.php

--- phpunit/multiplication_tests.php
+++ phpunit/multiplication_tests.php
@@ -1,1 +1,22 @@
 <?php
+
+use PHPUnit\Framework\TestCase;
+use Solution2\Solution;
+
+final class MultiplicationTest extends TestCase {
+
+    public function testTwoParams() : void {
+        $this->assertEquals(
+            solution::multiplication("3*4"),
+            12
+        );
+    }
+
+    public function testThreeParams() : void {
+        $this->assertEquals(
+            solution::multiplication("5*5*7"),
+            175
+        );
+    }
+
+}
--- solution2.php
+++ solution2.php
@@ -1,1 +1,54 @@
 <?php
+
+namespace Solution2;
+
+class Solution {
+    function eq(string $exp) : int {
+        static::multiplication($exp);
+
+
+        return 0;
+    }
+
+    /**
+     * Takes an expression, checks if there's any multiplication, and does the multiplication.
+     *
+     * @param string $exp
+     *
+     * @return int
+     */
+    public static function multiplication(string $exp) : ?int {
+        $result = NULL;
+        $numbers_to_be_multiplied = explode("*", $exp);
+
+        // The count will be >1 if there is anything to be multiplied.
+        if (count($numbers_to_be_multiplied) > 1) {
+            foreach ($numbers_to_be_multiplied as $key => $number_to_be_multiplied) {
+
+                // Set the first element and continue, otherwise it will multiply against itself.
+                if ($key == 0) {
+                    $result = $number_to_be_multiplied;
+                    continue;
+                }
+
+                $result = trim($result) * trim($number_to_be_multiplied);
+            }
+        }
+
+        return $result;
+    }
+
+    /**
+     * Check if ready to be evaluated.
+     */
+    function check($array, $char) {
+        $symbols = ['+', '-', '*', '/'];
+
+        $arr = str_split($array, 1);
+        foreach ($symbols as $symbol) {
+            if (in_array($symbol, $arr)) {
+
+            }
+        }
+    }
+}
